f101bbcbfbfe81fc5abced8ee11ceca461c62289
Starscourge <starscourge.sh@gmail.com>
2026-06-03 10:35:59 +0200

    Add: anagram-groups - submission-21

---
+++ Data Structures & Algorithms/anagram-groups/submission-21.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        def get_aschii_list(s: str) -> [List]:
+            ascii_list = []
+            for char in s:
+                ascii_list.append(str(ord(char)))
+
+            ascii_list.sort()
+            return ascii_list
+        
+        groups = []
+        done = []
+        for str1 in strs:
+            curr_str_ascii = get_aschii_list(str1)
+            if curr_str_ascii in done:
+                continue
+
+            group = []
+            for str2 in strs:
+                if curr_str_ascii == get_aschii_list(str2):
+                    group.append(str2)
+
+            groups.append(group)
+            done.append(curr_str_ascii)
+        return groups
+
+
+
+        
