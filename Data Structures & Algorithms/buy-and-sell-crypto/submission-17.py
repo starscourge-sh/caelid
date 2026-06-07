77ebd11779715925ab6e142408e18d9cc69ba2db
Starscourge <starscourge.sh@gmail.com>
2026-06-07 13:27:11 +0200

    Add: buy-and-sell-crypto - submission-17

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-17.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        for i, price in enumerate(prices):
+            if i == (len(prices) - 1):
+                return max_profit
+            future_prices = prices[i+1:]
+            projected_profit = max(future_prices) - price
+            if projected_profit > max_profit:
+                max_profit = projected_profit
+        return max_profit
+            
+        
