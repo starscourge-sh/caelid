5a57bdda48c051c14a50d7d1625d6e91253291ee
Starscourge <starscourge.sh@gmail.com>
2026-08-24 14:46:13 +0200

    Add: two-integer-sum - submission-45

---
+++ Data Structures & Algorithms/two-integer-sum/submission-45.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        for (let i = 0; i < nums.length; i++) {
+            let cur: number = nums[i];
+            let indexOfComplement: number = nums.lastIndexOf(target - cur);
+            // not a true pair, is same number or
+            // complementary number not in list
+            if (indexOfComplement == i || indexOfComplement == -1) continue;
+            return [i, indexOfComplement];
+        }
+    }
+}
